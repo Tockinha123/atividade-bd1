82b7cc7184f4f781729efadf1e2ca10a120f602a
VItor Andrade <150646534+Tockinha123@users.noreply.github.com>
2024-09-08 18:25:03 -0300

    Alteração na documentação da API.

--- user_registry/app.py
+++ user_registry/app.py
@@ -4,14 +4,24 @@ from fastapi import FastAPI, HTTPException
 
 from user_registry.schemas import UserDB, UserList, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(
+    title='DOCUMENTAÇÃO DA ATIVIDADE DE BD1',
+    description=(
+        'Cadastro de usuário simples, onde podemos criar um usuário. '
+        'Além disso podemos verificar todos os usuários no banco de dados,'
+        'e por fim podemos verificar um usuário de acordo com seu ID.'
+    ),
+)
 
 
 database = []
 
 
 @app.post(
-    '/create_user/', status_code=HTTPStatus.CREATED, response_model=UserPublic
+    '/create_user/',
+    status_code=HTTPStatus.CREATED,
+    response_model=UserPublic,
+    tags=['Cadastro de Usuário'],
 )
 def create_user(user: UserSchema):
     db_user = UserDB(
@@ -24,7 +34,12 @@ def create_user(user: UserSchema):
     return db_user
 
 
-@app.get('/read_users/', status_code=HTTPStatus.OK, response_model=UserList)
+@app.get(
+    '/read_users/',
+    status_code=HTTPStatus.OK,
+    response_model=UserList,
+    tags=['Cadastro de Usuário'],
+)
 def read_users():
     return {'users': database}
 
@@ -33,6 +48,7 @@ def read_users():
     '/read_user/{user_id}',
     status_code=HTTPStatus.OK,
     response_model=UserPublic,
+    tags=['Cadastro de Usuário'],
 )
 def read_user(user_id: int):
     if user_id > 0 and user_id <= len(database):
